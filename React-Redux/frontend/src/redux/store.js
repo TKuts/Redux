db047164ca52b606ef43ec50a7603bb569967dae
Anton Kuts <anton.a.kuts@gmail.com>
2024-01-25 20:54:36 +0200

    added errorSlice

--- React-Redux/frontend/src/redux/store.js
+++ React-Redux/frontend/src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import filterReducer from "./slices/filterSlice";
 import booksReducer from "./slices/booksSlice";
+import errorReducer from "./slices/erroSlice";
 
 const store = configureStore({
   reducer: {
     books: booksReducer,
     filter: filterReducer,
+    error: errorReducer,
   },
 });
 
